147a55de75048648d0682bdbaea143dbaf4c901c
João Pereira <66644767+ratzPereira@users.noreply.github.com>
2022-01-17 13:05:39 -0100

    added more configuration to gateway and created fallback methods

---
+++ gateway/src/main/java/com/ratz/gateway/controller/FallBackMethodController.java
@@ -0,0 +1,18 @@
+package com.ratz.gateway.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FallBackMethodController {
+
+  @GetMapping("userServiceFallBack")
+  public String userServiceFallBackMethod () {
+    return "User service is taking longer than expected. Please try again later.";
+  }
+
+  @GetMapping("departmentServiceFallBack")
+  public String departmentServiceFallBackMethod () {
+    return "Department service is taking longer than expected. Please try again later.";
+  }
+}
